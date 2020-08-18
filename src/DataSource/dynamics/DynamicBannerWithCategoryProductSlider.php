ba32f558fe51806f6fad6f764bc7335683469029
Krzysztof Kościuszko <krzysztof@axldynamics.pl>
2020-08-18 15:52:52 +0200

    dbwcps upd

--- src/DataSource/dynamics/DynamicBannerWithCategoryProductSlider.php
+++ src/DataSource/dynamics/DynamicBannerWithCategoryProductSlider.php
@@ -1,32 +1,54 @@
 <?php
 
-    namespace radoslawkoziol\SmartContent\DataSource\dynamics;
+namespace radoslawkoziol\SmartContent\DataSource\dynamics;
 
-    use App\Models\AccountCategory;
-    use App\Models\UnifiedCategory;
-    use radoslawkoziol\SmartContent\DataSource\AbstractDynamicDataSource;
+use App\Models\AccountCategory;
+use App\Models\UnifiedCategory;
+use App\Repositories\UnifiedCategoryRepository;
+use radoslawkoziol\SmartContent\DataSource\AbstractDynamicDataSource;
 
-    class DynamicBannerWithCategoryProductSlider extends AbstractDynamicDataSource {
+class DynamicBannerWithCategoryProductSlider extends AbstractDynamicDataSource {
 
-        /** @var AccountCategory $category */
-        public $category;
-        public $products = [];
-        public $title;
-        public $banner_image;
-        public $banner_mobile_image;
-        public $href;
+    /** @var UnifiedCategory|null $category */
+    public $category;
+    public $products = [];
+    public $title;
+    public $banner_image;
+    public $banner_mobile_image;
+    public $category_url;
 
-        public function __construct(int $prod_cat_id, int $account_id) {
+    public function __construct($slug) {
 
-            $this->null = false;
-            $this->setAccountId($account_id);
-            $this->category = AccountCategory::select('*')
-                        ->where('prod_cat_id', '=', $prod_cat_id)
-                        ->where('remote_account_id', '=', $this->getAccountId())
-                        ->first();
+        $this->setAccountId(-1);
+        $this->category = resolve(UnifiedCategoryRepository::class)->getBySlugOrId(-1, $slug);
 
-        }
+        if(!$this->category) return;
+        $this->products = $this->category->getBestProductsGlobal();
+        $this->null = false;
+        // $this->title = $this->category->getProdCatLangTitle();
+        $this->category_url = route('products.list', ['slug' => $slug]);
+
+
+        // $this->setAccountId($account_id);
+        // $builder = AccountCategory::select('*')->where('prod_cat_id', 'in', $prod_cat_ids);
+        // if($account_id != -1) {
+        //     $builder->where('remote_account_id', '=', $this->getAccountId());
+        // }
+        //
+        // $this->category = $builder->first();
+        // if($this->category) {
+        //     $this->null = false;
+        //     $this->category_url = routeWithDomain('products.list', ['slug' => $this->category->slug]);
+        //     if(!$this->title) {
+        //         $this->title = $this->category->prod_cat_lang_title;
+        //     }
+        // }
+
 
 
 
     }
+
+
+
+}
