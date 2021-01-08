0a2d620b50c52fedabae38b63023d8e1ff5bd4c6
Radosław Kozioł <koziol.radek@gmail.com>
2021-01-08 13:51:01 +0100

    1.1.2

--- src/DataSource/dynamics/DynamicBannerWithCategoryProductSlider.php
+++ src/DataSource/dynamics/DynamicBannerWithCategoryProductSlider.php
@@ -4,11 +4,12 @@ namespace radoslawkoziol\SmartContent\DataSource\dynamics;
 
 use App\Models\AccountCategory;
 use App\Models\UnifiedCategory;
+use App\Repositories\UnifiedCategoryRepository;
 use radoslawkoziol\SmartContent\DataSource\AbstractDynamicDataSource;
 
 class DynamicBannerWithCategoryProductSlider extends AbstractDynamicDataSource {
 
-    /** @var AccountCategory $category */
+    /** @var UnifiedCategory|null $category */
     public $category;
     public $products = [];
     public $title;
@@ -16,25 +17,15 @@ class DynamicBannerWithCategoryProductSlider extends AbstractDynamicDataSource {
     public $banner_mobile_image;
     public $category_url;
 
-    public function __construct(int $prod_cat_id, int $account_id = -1) {
+    public function __construct($slug) {
 
-        $this->setAccountId($account_id);
-        $builder = AccountCategory::select('*')->where('prod_cat_id', '=', $prod_cat_id);
-        if($account_id != -1) {
-            $builder->where('remote_account_id', '=', $this->getAccountId());
-        }
+        $this->setAccountId(-1);
+        $this->category = resolve(UnifiedCategoryRepository::class)->getBySlugOrId(-1, $slug);
 
-        $this->category = $builder->first();
-        if($this->category) {
-            $this->null = false;
-            $this->category_url = routeWithDomain('products.list', ['slug' => $this->category->slug]);
-            if(!$this->title) {
-                $this->title = $this->category->prod_cat_lang_title;
-            }
-        }
-
-
-
+        if(!$this->category) return;
+        $this->products = $this->category->getBestProductsGlobal();
+        $this->null = false;
+        $this->category_url = routeWithDomain('products.list', ['slug' => $slug]);
 
     }
 
